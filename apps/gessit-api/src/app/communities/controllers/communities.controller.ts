80c67b7dfc173c058fdfc2b5b74d5dcc63dcf383
GuusvD <g.vandamme@student.avans.nl>
2022-11-17 20:24:32 +0100

    Added new community schema and added crud for it

---
+++ apps/gessit-api/src/app/communities/controllers/communities.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CommunitiesService } from '../services/communities.service';
+import { CreateCommunityDto } from '../dto/create-community.dto';
+import { Community } from '../schemas/community.schema';
+import { UpdateCommunityDto } from '../dto/update-community.dto';
+
+@Controller('community')
+export class CommunitiesController {
+  constructor(private readonly communityService: CommunitiesService) {}
+
+  @Get()
+  async getCommunities(): Promise<Community[]> {
+    return await this.communityService.getCommunities();
+  }
+
+  @Get(':id')
+  async getCommunityById(@Param('id') id: string): Promise<Community> {
+    return await this.communityService.getCommunityById(id);
+  }
+
+  @Post()
+  async createCommunity(@Body() createCommunityDto: CreateCommunityDto): Promise<Community> {
+    return await this.communityService.createCommunity(createCommunityDto.name, createCommunityDto.description);
+  }
+
+  @Patch(':id')
+  async updateCommunity(@Param('id') id: string, @Body() updateCommunityDto: UpdateCommunityDto): Promise<Community> {
+    return await this.communityService.updateCommunity(id, updateCommunityDto);
+  }
+
+  @Delete(':id')
+  async deleteCommunity(@Param('id') id: string): Promise<Community> {
+    return await this.communityService.deleteCommunity(id);
+  }
+}
--- dist/apps/gessit-api/main.js
+++ dist/apps/gessit-api/main.js
@@ -21,7 +21,7 @@ let AppController = class AppController {
     }
 };
 tslib_1.__decorate([
-    (0, common_1.Get)('/hello'),
+    (0, common_1.Get)(),
     tslib_1.__metadata("design:type", Function),
     tslib_1.__metadata("design:paramtypes", []),
     tslib_1.__metadata("design:returntype", void 0)
@@ -43,13 +43,15 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.AppModule = void 0;
 const tslib_1 = __webpack_require__("tslib");
 const common_1 = __webpack_require__("@nestjs/common");
+const mongoose_1 = __webpack_require__("@nestjs/mongoose");
 const app_controller_1 = __webpack_require__("./apps/gessit-api/src/app/app.controller.ts");
 const app_service_1 = __webpack_require__("./apps/gessit-api/src/app/app.service.ts");
+const communities_module_1 = __webpack_require__("./apps/gessit-api/src/app/communities/communities.module.ts");
 let AppModule = class AppModule {
 };
 AppModule = tslib_1.__decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [mongoose_1.MongooseModule.forRoot('mongodb://127.0.0.1:27017/gessit'), communities_module_1.CommunitiesModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
@@ -80,6 +82,293 @@ exports.AppService = AppService;
 
 /***/ }),
 
+/***/ "./apps/gessit-api/src/app/communities/communities.module.ts":
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.CommunitiesModule = void 0;
+const tslib_1 = __webpack_require__("tslib");
+const mongoose_1 = __webpack_require__("@nestjs/mongoose");
+const common_1 = __webpack_require__("@nestjs/common");
+const community_schema_1 = __webpack_require__("./apps/gessit-api/src/app/communities/schemas/community.schema.ts");
+const communities_controller_1 = __webpack_require__("./apps/gessit-api/src/app/communities/controllers/communities.controller.ts");
+const communities_service_1 = __webpack_require__("./apps/gessit-api/src/app/communities/services/communities.service.ts");
+const communities_repository_1 = __webpack_require__("./apps/gessit-api/src/app/communities/repositories/communities.repository.ts");
+let CommunitiesModule = class CommunitiesModule {
+};
+CommunitiesModule = tslib_1.__decorate([
+    (0, common_1.Module)({
+        imports: [mongoose_1.MongooseModule.forFeature([{ name: community_schema_1.Community.name, schema: community_schema_1.CommunitySchema }])],
+        controllers: [communities_controller_1.CommunitiesController],
+        providers: [communities_service_1.CommunitiesService, communities_repository_1.CommunitiesRepository]
+    })
+], CommunitiesModule);
+exports.CommunitiesModule = CommunitiesModule;
+
+
+/***/ }),
+
+/***/ "./apps/gessit-api/src/app/communities/controllers/communities.controller.ts":
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+var _a, _b, _c, _d, _e, _f, _g, _h;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.CommunitiesController = void 0;
+const tslib_1 = __webpack_require__("tslib");
+const common_1 = __webpack_require__("@nestjs/common");
+const communities_service_1 = __webpack_require__("./apps/gessit-api/src/app/communities/services/communities.service.ts");
+const create_community_dto_1 = __webpack_require__("./apps/gessit-api/src/app/communities/dto/create-community.dto.ts");
+const update_community_dto_1 = __webpack_require__("./apps/gessit-api/src/app/communities/dto/update-community.dto.ts");
+let CommunitiesController = class CommunitiesController {
+    constructor(communityService) {
+        this.communityService = communityService;
+    }
+    getCommunities() {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return yield this.communityService.getCommunities();
+        });
+    }
+    getCommunityById(id) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return yield this.communityService.getCommunityById(id);
+        });
+    }
+    createCommunity(createCommunityDto) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return yield this.communityService.createCommunity(createCommunityDto.name, createCommunityDto.description);
+        });
+    }
+    updateCommunity(id, updateCommunityDto) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return yield this.communityService.updateCommunity(id, updateCommunityDto);
+        });
+    }
+    deleteCommunity(id) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return yield this.communityService.deleteCommunity(id);
+        });
+    }
+};
+tslib_1.__decorate([
+    (0, common_1.Get)(),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", []),
+    tslib_1.__metadata("design:returntype", typeof (_b = typeof Promise !== "undefined" && Promise) === "function" ? _b : Object)
+], CommunitiesController.prototype, "getCommunities", null);
+tslib_1.__decorate([
+    (0, common_1.Get)(':id'),
+    tslib_1.__param(0, (0, common_1.Param)('id')),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", [String]),
+    tslib_1.__metadata("design:returntype", typeof (_c = typeof Promise !== "undefined" && Promise) === "function" ? _c : Object)
+], CommunitiesController.prototype, "getCommunityById", null);
+tslib_1.__decorate([
+    (0, common_1.Post)(),
+    tslib_1.__param(0, (0, common_1.Body)()),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", [typeof (_d = typeof create_community_dto_1.CreateCommunityDto !== "undefined" && create_community_dto_1.CreateCommunityDto) === "function" ? _d : Object]),
+    tslib_1.__metadata("design:returntype", typeof (_e = typeof Promise !== "undefined" && Promise) === "function" ? _e : Object)
+], CommunitiesController.prototype, "createCommunity", null);
+tslib_1.__decorate([
+    (0, common_1.Patch)(':id'),
+    tslib_1.__param(0, (0, common_1.Param)('id')),
+    tslib_1.__param(1, (0, common_1.Body)()),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", [String, typeof (_f = typeof update_community_dto_1.UpdateCommunityDto !== "undefined" && update_community_dto_1.UpdateCommunityDto) === "function" ? _f : Object]),
+    tslib_1.__metadata("design:returntype", typeof (_g = typeof Promise !== "undefined" && Promise) === "function" ? _g : Object)
+], CommunitiesController.prototype, "updateCommunity", null);
+tslib_1.__decorate([
+    (0, common_1.Delete)(':id'),
+    tslib_1.__param(0, (0, common_1.Param)('id')),
+    tslib_1.__metadata("design:type", Function),
+    tslib_1.__metadata("design:paramtypes", [String]),
+    tslib_1.__metadata("design:returntype", typeof (_h = typeof Promise !== "undefined" && Promise) === "function" ? _h : Object)
+], CommunitiesController.prototype, "deleteCommunity", null);
+CommunitiesController = tslib_1.__decorate([
+    (0, common_1.Controller)('community'),
+    tslib_1.__metadata("design:paramtypes", [typeof (_a = typeof communities_service_1.CommunitiesService !== "undefined" && communities_service_1.CommunitiesService) === "function" ? _a : Object])
+], CommunitiesController);
+exports.CommunitiesController = CommunitiesController;
+
+
+/***/ }),
+
+/***/ "./apps/gessit-api/src/app/communities/dto/create-community.dto.ts":
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.CreateCommunityDto = void 0;
+class CreateCommunityDto {
+}
+exports.CreateCommunityDto = CreateCommunityDto;
+
+
+/***/ }),
+
+/***/ "./apps/gessit-api/src/app/communities/dto/update-community.dto.ts":
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.UpdateCommunityDto = void 0;
+class UpdateCommunityDto {
+}
+exports.UpdateCommunityDto = UpdateCommunityDto;
+
+
+/***/ }),
+
+/***/ "./apps/gessit-api/src/app/communities/repositories/communities.repository.ts":
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+var _a;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.CommunitiesRepository = void 0;
+const tslib_1 = __webpack_require__("tslib");
+const common_1 = __webpack_require__("@nestjs/common");
+const mongoose_1 = __webpack_require__("@nestjs/mongoose");
+const mongoose_2 = __webpack_require__("mongoose");
+const community_schema_1 = __webpack_require__("./apps/gessit-api/src/app/communities/schemas/community.schema.ts");
+let CommunitiesRepository = class CommunitiesRepository {
+    constructor(communityModel) {
+        this.communityModel = communityModel;
+    }
+    findOne(communityFilterQuery) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityModel.findOne(communityFilterQuery);
+        });
+    }
+    find(communitiesFilterQuery) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityModel.find(communitiesFilterQuery);
+        });
+    }
+    create(community) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            const newCommunity = new this.communityModel(community);
+            return newCommunity.save();
+        });
+    }
+    findOneAndUpdate(communityFilterQuery, community) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityModel.findOneAndUpdate(communityFilterQuery, community, { new: true });
+        });
+    }
+    findOneAndDelete(communityFilterQuery) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityModel.findOneAndDelete(communityFilterQuery);
+        });
+    }
+};
+CommunitiesRepository = tslib_1.__decorate([
+    (0, common_1.Injectable)(),
+    tslib_1.__param(0, (0, mongoose_1.InjectModel)(community_schema_1.Community.name)),
+    tslib_1.__metadata("design:paramtypes", [typeof (_a = typeof mongoose_2.Model !== "undefined" && mongoose_2.Model) === "function" ? _a : Object])
+], CommunitiesRepository);
+exports.CommunitiesRepository = CommunitiesRepository;
+
+
+/***/ }),
+
+/***/ "./apps/gessit-api/src/app/communities/schemas/community.schema.ts":
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+var _a, _b;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.CommunitySchema = exports.Community = void 0;
+const tslib_1 = __webpack_require__("tslib");
+const mongoose_1 = __webpack_require__("@nestjs/mongoose");
+let Community = class Community {
+};
+tslib_1.__decorate([
+    (0, mongoose_1.Prop)(),
+    tslib_1.__metadata("design:type", String)
+], Community.prototype, "name", void 0);
+tslib_1.__decorate([
+    (0, mongoose_1.Prop)(),
+    tslib_1.__metadata("design:type", String)
+], Community.prototype, "description", void 0);
+tslib_1.__decorate([
+    (0, mongoose_1.Prop)(),
+    tslib_1.__metadata("design:type", typeof (_a = typeof Number !== "undefined" && Number) === "function" ? _a : Object)
+], Community.prototype, "ranking", void 0);
+tslib_1.__decorate([
+    (0, mongoose_1.Prop)(),
+    tslib_1.__metadata("design:type", typeof (_b = typeof Date !== "undefined" && Date) === "function" ? _b : Object)
+], Community.prototype, "creationDate", void 0);
+tslib_1.__decorate([
+    (0, mongoose_1.Prop)(),
+    tslib_1.__metadata("design:type", String)
+], Community.prototype, "image", void 0);
+Community = tslib_1.__decorate([
+    (0, mongoose_1.Schema)()
+], Community);
+exports.Community = Community;
+exports.CommunitySchema = mongoose_1.SchemaFactory.createForClass(Community);
+
+
+/***/ }),
+
+/***/ "./apps/gessit-api/src/app/communities/services/communities.service.ts":
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+var _a;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.CommunitiesService = void 0;
+const tslib_1 = __webpack_require__("tslib");
+const common_1 = __webpack_require__("@nestjs/common");
+const communities_repository_1 = __webpack_require__("./apps/gessit-api/src/app/communities/repositories/communities.repository.ts");
+let CommunitiesService = class CommunitiesService {
+    constructor(communityRepository) {
+        this.communityRepository = communityRepository;
+    }
+    getCommunityById(id) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityRepository.findOne({ _id: id });
+        });
+    }
+    getCommunities() {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityRepository.find({});
+        });
+    }
+    createCommunity(name, description) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityRepository.create({
+                name,
+                description,
+                ranking: 0,
+                creationDate: new Date(),
+                image: "url"
+            });
+        });
+    }
+    updateCommunity(id, community) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityRepository.findOneAndUpdate({ _id: id }, community);
+        });
+    }
+    deleteCommunity(id) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.communityRepository.findOneAndDelete({ _id: id });
+        });
+    }
+};
+CommunitiesService = tslib_1.__decorate([
+    (0, common_1.Injectable)(),
+    tslib_1.__metadata("design:paramtypes", [typeof (_a = typeof communities_repository_1.CommunitiesRepository !== "undefined" && communities_repository_1.CommunitiesRepository) === "function" ? _a : Object])
+], CommunitiesService);
+exports.CommunitiesService = CommunitiesService;
+
+
+/***/ }),
+
 /***/ "@nestjs/common":
 /***/ ((module) => {
 
@@ -91,6 +380,20 @@ module.exports = require("@nestjs/common");
 /***/ ((module) => {
 
 module.exports = require("@nestjs/core");
+
+/***/ }),
+
+/***/ "@nestjs/mongoose":
+/***/ ((module) => {
+
+module.exports = require("@nestjs/mongoose");
+
+/***/ }),
+
+/***/ "mongoose":
+/***/ ((module) => {
+
+module.exports = require("mongoose");
 
 /***/ }),
 
