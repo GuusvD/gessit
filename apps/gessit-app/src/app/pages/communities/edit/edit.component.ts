dc543c65f34a88069bfa6ca9f09899d39da905fa
GuusvD <g.vandamme@student.avans.nl>
2022-12-06 19:56:22 +0100

    Created themeservice, edited communitiesservice, changed entity, fixed create community function and made start for update community

--- apps/gessit-app/src/app/pages/communities/edit/edit.component.ts
+++ apps/gessit-app/src/app/pages/communities/edit/edit.component.ts
@@ -1,45 +1,143 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Community } from 'libs/data/src/entities/community';
+import { Theme } from 'libs/data/src/entities/theme';
 import { Subscription } from 'rxjs';
 import { CommunitiesService } from '../../../../../../../libs/data/src/services/communities.service';
+import { ThemesService } from '../../../../../../../libs/data/src/services/themes.service';
 
 @Component({
   selector: 'gessit-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css'],
 })
-export class EditComponent implements OnInit {
+export class EditComponent implements OnInit, OnDestroy {
   title: string | undefined;
   createCommunity: boolean | undefined;
-  newCommunity = new Community();
-  subscription: Subscription | undefined;
-  communityId: string | null = null;
-  community: Community | undefined;
+  subs?: Subscription;
+  communityForm: FormGroup = new FormGroup({});
+  communityId : string | undefined;
+  themes: Theme[] | undefined;
+  selectedThemes: string[] | undefined;
 
-  constructor(private route: ActivatedRoute, private communitiesService: CommunitiesService, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private communitiesService: CommunitiesService,
+    private themesService: ThemesService
+  ) {}
 
   ngOnInit(): void {
     this.title = this.route.snapshot.data['title'] || undefined;
     this.createCommunity = this.route.snapshot.data['createCommunity'] || false;
 
-    if (!this.createCommunity) {
-      this.subscription = this.route.paramMap.subscribe(params => {
-        this.communityId = params.get('id');
-        if (this.communityId) {
-          this.communitiesService.getById(this.communityId).subscribe((c) => (this.community = c)).unsubscribe;
-        }
+    if(!this.createCommunity) {
+      this.subs = this.route.paramMap.subscribe((params) => {
+        this.communityId = params.get('id')?.toString()
       });
+
+      this.communityForm = new FormGroup({
+        name: new FormControl(null, [Validators.required, this.validName.bind(this)]),
+        description: new FormControl(null, [Validators.required, this.validDescription.bind(this)]),
+        image: new FormControl(null, [Validators.required, this.validImageUrl.bind(this)]),
+        isOpen: new FormControl(false),
+      });
+
+      this.subs = this.communitiesService.getById(this.communityId as string).subscribe((community) => {
+        this.communityForm.patchValue({name: community.name, description: community.description, imageUrl: community.image, isPublic: community.isOpen});
+      });
+    }
+    else {
+      this.communityForm = new FormGroup({
+        name: new FormControl(null, [Validators.required, this.validName.bind(this)]),
+        description: new FormControl(null, [Validators.required, this.validDescription.bind(this)]),
+        image: new FormControl(null, [Validators.required, this.validImageUrl.bind(this)]),
+        isOpen: new FormControl(false),
+      });
+    }
+
+    this.themesService.getThemes().subscribe((p) => (this.themes = p));
+    this.selectedThemes = [];
+  }
+
+  ngOnDestroy(): void {
+    if (this.subs) {
+      this.subs.unsubscribe();
     }
   }
 
-  create() {
-    this.communitiesService.create(this.newCommunity);
-    this.router.navigate(['/communities']);
+  onSubmit() {
+    if (this.communityForm.valid) {
+      const community = this.communityForm.value as Community;
+
+      if (this.selectedThemes) {
+        community.themes = this.selectedThemes;
+      }
+
+      if(!this.createCommunity) {
+        // this.authService.register(this.registerForm.value).subscribe((user) => {
+        //   if (user) {
+        //     console.log('user = ', user);
+        //     this.router.navigate(['/communities']);
+        //   }
+        // });
+      } else {
+        this.communitiesService.create(this.communityForm.value).subscribe((community) => {
+          if (community) {
+            this.router.navigate(['/communities']);
+          }
+        });
+      }
+    }
   }
 
-  update() {
-    this.communitiesService.update(this.community!);
-    this.router.navigate([`/communities/${this.communityId}`]);
+  onChange(_id: string) {
+    if (this.selectedThemes?.includes(_id)) {
+      this.selectedThemes = this.selectedThemes.filter(p => p !== _id);
+    } else {
+      this.selectedThemes?.push(_id);
+    }
+  }
+
+  validImageUrl(control: FormControl): { [s: string]: boolean } {
+    const imageUrl = control.value;
+    const regexp = new RegExp('^(https?:\\/\\/)?'+
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ 
+    '(\\#[-a-z\\d_]*)?$','i');
+    if (regexp.test(imageUrl) !== true) {
+      return { imageUrl: false };
+    } else {
+      return null!
+    }
+  }
+
+  validName(control: FormControl): { [s: string]: boolean } {
+    const name = control.value;
+    const regexp = new RegExp(
+      '.{5,}'
+    );
+
+    if (regexp.test(name) !== true) {
+      return { name: false };
+    } else {
+      return null!;
+    }
+  }
+
+  validDescription(control: FormControl): { [s: string]: boolean } {
+    const description = control.value;
+    const regexp = new RegExp(
+      '.{10,}'
+    );
+
+    if (regexp.test(description) !== true) {
+      return { description: false };
+    } else {
+      return null!;
+    }
   }
 }
--- libs/data/src/services/communities.service.ts
+++ libs/data/src/services/communities.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
 import { Community } from "libs/data/src/entities/community";
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from "../environments/environment";
-import { Observable } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 import { AuthService } from '../../../../apps/gessit-app/src/app/auth/auth.service'
+import { AlertService } from "apps/gessit-app/src/app/shared/alert/alert.service";
 
 @Injectable({providedIn: 'root',})
 export class CommunitiesService {
     private community? : Community;
 
-    constructor(private httpClient: HttpClient, private authService: AuthService) {}
+    constructor(private httpClient: HttpClient, private authService: AuthService, private alertService: AlertService) {}
 
     getCommunities(endpoint: string): Observable<Community[]> {
         const httpOptions = this.authService.formHeaders();
@@ -17,35 +18,57 @@ export class CommunitiesService {
     }
 
     getById(communityId: string): Observable<Community> {
-        return this.httpClient.get<Community>(environment.BASE_API_URL + `community/${communityId}`) as Observable<Community>;
+        const httpOptions = this.authService.formHeaders();
+        return this.httpClient.get<Community>(environment.BASE_API_URL + `community/${communityId}`, httpOptions) as Observable<Community>;
     }
 
-    create(community: Community) {
-        this.community = new Community();
-
-        this.community.name = community.name;
-        this.community.description = community.description;
-        this.community.image = community.image;
-
-        if (community.isOpen == undefined) {
-            this.community.isOpen = false;
-        } else {
-            this.community.isOpen = community.isOpen;
-        }
-
-        this.httpClient.post<Community>(environment.BASE_API_URL + 'community', this.community).subscribe();
+    create(communityData: Community): Observable<Community | undefined> {
+        console.log('creating community at' + environment.BASE_API_URL + 'community');
+        console.log(communityData);
+  
+        return this.httpClient
+            .post<Community>(
+                environment.BASE_API_URL + 'community', 
+                communityData,
+                this.authService.formHeaders()
+            )
+            .pipe(
+                map((community) => {
+                    console.dir(community);
+                    this.alertService.success('Community has been created');
+                    return community;
+                }),
+                catchError((error: any) => {
+                    console.log('error:', error);
+                    console.log('error.message:', error.message);
+                    console.log('error.error.message:', error.error.message);
+                    this.alertService.error(error.error.message || error.message);
+                    return of(undefined); 
+                })
+            );
     }
     
-    update(community: Community) {
-        this.community = new Community();
-
-        this.community._id = community._id;
-        this.community.name = community.name;
-        this.community.description = community.description;
-        this.community.image = community.image;
-        this.community.isOpen = community.isOpen;
-
-        this.httpClient.patch<Community>(environment.BASE_API_URL + `community/${community._id}`, this.community).subscribe();
+    update(communityData: Community, communityId : string): Observable<Community | undefined> {
+        console.log(`updating community at ${environment.BASE_API_URL}communities`);
+      
+        return this.httpClient
+          .patch<Community>(`${environment.BASE_API_URL}communities/${communityId}`, communityData,
+            this.authService.formHeaders()
+          )
+          .pipe(
+            map((community) => {
+              console.dir(community);
+              this.alertService.success('Community has been updated');
+              return community;
+            }),
+            catchError((error: any) => {
+              console.log('error:', error);
+              console.log('error.message:', error.message);
+              console.log('error.error.message:', error.error.message);
+              this.alertService.error(error.error.message || error.message);
+              return of(undefined); 
+            })
+        );
     }
 
     delete(communityId: string) {
