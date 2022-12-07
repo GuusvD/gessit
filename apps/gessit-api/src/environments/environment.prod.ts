180315f57744098cbd447985db3f8618fe675e9f
GuusvD <g.vandamme@student.avans.nl>
2022-12-07 18:45:14 +0100

    Hosting changes

--- apps/gessit-api/src/environments/environment.prod.ts
+++ apps/gessit-api/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   BASE_MONGO_URL: 'mongodb://127.0.0.1:27017/gessit',
-  BASE_NEO_HOST: '127.0.0.1',
-  BASE_API_URL: 'http://localhost:3333/api/'
+  BASE_NEO_HOST: '127.0.0.1'
 };
--- dist/apps/gessit-api/main.js
+++ dist/apps/gessit-api/main.js
@@ -2905,8 +2905,7 @@ exports.environment = void 0;
 exports.environment = {
     production: true,
     BASE_MONGO_URL: 'mongodb://127.0.0.1:27017/gessit',
-    BASE_NEO_HOST: '127.0.0.1',
-    BASE_API_URL: 'http://localhost:3333/api/'
+    BASE_NEO_HOST: '127.0.0.1'
 };
 
 
